c553a514241e5f0ca4e74a40c3bda0c7e6d97fd6
nikicyde <niki.szabo6@gmail.com>
2022-10-31 10:45:12 +0100

    t

--- test.py
+++ test.py
@@ -10,6 +10,7 @@ class Test(unittest.TestCase):
 		options.add_argument('--no-sandbox')
 		options.add_argument('--headless')
 		options.add_argument('--disable-dev-shm-usage')
+		options.add_argument('--disable-gpu')        
 		driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 		driver.get("http://localhost:31001/")
 		h1 = driver.find_element("id","hello").text
