c40fc657db6c21fdf709fbd0b1d2c06a41e9648f
nikicyde <niki.szabo6@gmail.com>
2022-10-31 10:38:00 +0100

    ttt

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 class Test(unittest.TestCase):
 	def testName(self):
 		options = Options()
-		options.add_argument('--headless')
 		options.add_argument('--no-sandbox')
+		options.add_argument('--headless')
 		options.add_argument('--disable-dev-shm-usage')
 		driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 		driver.get("http://localhost:31001/")
