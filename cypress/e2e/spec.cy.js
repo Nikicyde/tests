50234014004b2694b6c484a3bf8f2eedadff6c72
nikicyde <niki.szabo6@gmail.com>
2024-06-19 12:34:06 +0200

    change url

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe('empty spec', () => {
   it('passes', () => {
-    cy.visit('http://localhost:31001/')
+    cy.visit('http://localhost:31025/')
     cy.get('#hello')
     .should('have.text', 'Hello Krone!')
   })
